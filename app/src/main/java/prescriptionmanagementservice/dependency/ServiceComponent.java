7bdd3038abe78c2981c7d7fc6ab0a09ca16a87c7
Kaden <kadenc@charter.net>
2023-05-22 11:55:32 -0500

    Created directories as well as initial classes

---
+++ app/src/main/java/prescriptionmanagementservice/dependency/ServiceComponent.java
@@ -0,0 +1,14 @@
+package prescriptionmanagementservice.dependency;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger component for providing dependency injection in the Music Playlist Service.
+ */
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
+public interface ServiceComponent {
+
+}
